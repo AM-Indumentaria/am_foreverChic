da8309c401d1329a9c7dc6918c1254cac15094fe
Flavia S. Briglia <fbrig87@gmail.com>
2023-10-20 16:08:28 -0300

    cambia png por webp

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -6,9 +6,9 @@ export const CarouselAm = ()=>{
 
     return(
         <Carousel interval={6000}>
-            <img className="img-carousel" src=".\src\assets\carousel_img_1.png" alt="imagen 1" />
-            <img className="img-carousel" src=".\src\assets\carousel_img_2.png" alt="imagen 2" />
-            <img className="img-carousel" src=".\src\assets\carousel_img_3.png" alt="imagen 3" />
+            <img className="img-carousel" src=".\src\assets\carousel_img_1.webp" alt="imagen 1" />
+            <img className="img-carousel" src=".\src\assets\carousel_img_2.webp" alt="imagen 2" />
+            <img className="img-carousel" src=".\src\assets\carousel_img_3.webp" alt="imagen 3" />
         </Carousel>
     )
 }
