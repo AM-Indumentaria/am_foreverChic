5d5d297f56722292706f0066f1eab89f5d10f0ed
Flavia S. Briglia <fbrig87@gmail.com>
2023-10-23 16:11:56 -0300

    cambia rutas de layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { NavbarContainer } from "./Components/layout/navbar/NavbarContainer";
+import { NavbarContainer } from "./Components/navbar/NavbarContainer";
 import { CarouselAm } from "./Components/Carousel/Carousel";
 import { Moda } from "./Components/moda/Moda";
 import { GuiaTalles } from "./Components/talles/GuiaTalles";
 import { QuienesSomos } from "./Components/quienes_somos/QuienesSomos";
 import { AccordionAm } from "./Components/Accordion/AccordionAm";
-import { Footer } from "./Components/layout/footer/Footer.jsx";
+import { Footer } from "./Components/footer/Footer.jsx";
 import { FloatButton } from "./Components/floatButton/FloatButton";
 
 function App() {
