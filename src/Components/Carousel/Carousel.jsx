962c772f2773789a56fe4d75ec3d124ae01dcf80
Flavia S. Briglia <fbrig87@gmail.com>
2023-10-23 16:23:24 -0300

    modifica enlace img carrusel

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,14 +1,17 @@
 import Carousel from "react-material-ui-carousel"
 import "./Carousel.css"
+import carousel_img_1 from "../../assets/carousel_img_1.webp";
+import carousel_img_2 from "../../assets/carousel_img_2.webp";
+import carousel_img_3 from "../../assets/carousel_img_3.webp";
 
 
 export const CarouselAm = ()=>{
 
-    return(
-        <Carousel interval={6000}>
-            <img className="img-carousel" src=".\src\assets\carousel_img_1.webp" alt="imagen 1" />
-            <img className="img-carousel" src=".\src\assets\carousel_img_2.webp" alt="imagen 2" />
-            <img className="img-carousel" src=".\src\assets\carousel_img_3.webp" alt="imagen 3" />
-        </Carousel>
-    )
+    return (
+      <Carousel interval={6000}>
+        <img className="img-carousel" src={carousel_img_1} alt="imagen 1" />
+        <img className="img-carousel" src={carousel_img_2} alt="imagen 2" />
+        <img className="img-carousel" src={carousel_img_3} alt="imagen 3" />
+      </Carousel>
+    );
 }
