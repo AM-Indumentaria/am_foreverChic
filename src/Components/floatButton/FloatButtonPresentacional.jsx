0143d363a1d5f5123924d9db3d4a5821d5cc86db
santiago arteche <santiarteche@hotmail.com>
2023-10-23 23:06:34 -0300

    floatButton mobileSize

--- src/Components/floatButton/FloatButtonPresentacional.jsx
+++ src/Components/floatButton/FloatButtonPresentacional.jsx
@@ -14,7 +14,7 @@ export const FloatButtonPresentacional = ({ active, activeIcon }) => {
             ":hover": { backgroundColor: "#E4405F" },
             position: "fixed",
             top: {
-              xs: "77vh",
+              xs: "71vh",
               sm: "75vh",
               md: "75vh",
               lg: "75vh",
@@ -82,7 +82,7 @@ export const FloatButtonPresentacional = ({ active, activeIcon }) => {
             },
 
             top: {
-              xs: "84vh",
+              xs: "78vh",
               sm: "83vh",
               md: "83vh",
               lg: "83vh",
@@ -137,7 +137,7 @@ export const FloatButtonPresentacional = ({ active, activeIcon }) => {
             ":hover": { backgroundColor: "#FFAD66" },
             position: "fixed",
             top: {
-              xs: "91vh",
+              xs: "85vh",
               sm: "91vh",
               md: "91vh",
               lg: "91vh",
